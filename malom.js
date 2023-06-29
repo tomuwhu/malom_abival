a3ca634276126b9ee5c9aa0de5b8f1d78871c39d
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-06-29 21:28:39 +0200

    .

--- malom.js
+++ malom.js
@@ -22,12 +22,34 @@ malom = [
         y: 25,
         b: "none",
         sz: [4, 10]
+    },
+    {
+        x: 60,
+        y: 25,
+        b: "none",
+        sz: [1, 3, 5]
+    },
+    {
+        x: 95,
+        y: 25,
+        b: "none",
+        sz: [4, 13]
     }
 ]
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("mt").innerHTML = malom.map((p, i) => 
         `<circle cx="${p.x}" cy="${p.y}" r="6" fill="none" stroke="black"></circle>`
-    ).join("")
+    ).join("") + 
+    malom.map((p, i) => p.sz.map(j => {
+        if (j < malom.length) {
+            x = [malom[j].x, p.x].sort((a, b) => a - b)
+            y = [malom[j].y, p.y].sort((a, b) => a - b)
+            if (x[0] == x[1]) y = [y[0]+6, y[1]-6]
+            else x = [x[0]+6, x[1]-6]
+            return  `<line x1="${x[0]}" y1="${y[0]}" x2="${x[1]}" y2="${y[1]}" stroke="black" />`
+        }
+    }).join("")). join("")
+    
 })
 
 
